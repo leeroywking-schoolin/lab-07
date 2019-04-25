10566b35a7c9d106dc25cac4197d7aac44609578
leeroywking <leeroywking@gmail.com>
2019-04-24 20:00:44 -0700

    not working yet

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 //Load express to do the heavey lifting
 const express = require('express');
 const app = express();
-
+const superagent = require('superagent');
 const cors = require('cors'); //Cross Origin Resource Sharing
 
 app.use(cors()); //tell express to use cors
@@ -16,23 +16,29 @@ const PORT = process.env.PORT;
 app.get('/testing', (request, response)=>{
   console.log('found the testing route')
   response.send('<h1>HELLO WORLD..</h1>')
-
 });
 
+app.get('/location', searchToLatLong);
 
-app.get('/location', (request, response)=>{
-  try{
-    const locationData = searchToLatLong(request.query.data);//hat
-    response.send(locationData)
-  }
+app.listen(PORT, ()=>console.log(`Listening on PORT ${PORT}`));
 
-  catch(error) {  
-    console.error(error);
-    response.status(500).send('Status: 500. So sorry, something went Wrong');
+function searchToLatLong(request, response){
+  console.log('this function has been called');
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`
+  console.log(request.query.data);
+  
+  superagent.get(url)
+  .then(resultJSON => {
+    const location = new Location(resultJSON.query.data, result) 
+    console.log(location);
+    response.send(location); })
+  .catch(err => handleError(err, response));
+}
 
-  }
-})
 
+
+
+// app.get('/weather', getWeather);
 app.get('/weather', (request, response)=>{
   try{
     console.log('From weather Request', request.query.data.latitude);
@@ -47,30 +53,22 @@ app.get('/weather', (request, response)=>{
 });
 
 
-app.listen(PORT, ()=>console.log(`Listening on PORT ${PORT}`));
 
 
 //Helper Functions
-function searchToLatLong(query){
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData);
-  console.log(location);
-  return location;
-}
 
 // Constructor for location data
-function Location(data) {
-  this.formatted_query = data.results[0].formatted_address;
-  this.latitude = data.results[0].geometry.location.lat;
-  this.longitude = data.results[0].geometry.location.lng;
+function Location(query, res) {
+  this.search_query = query
+  this.formatted_query = res.body.results[0].formatted_address;
+  this.latitude = res.body.results[0].geometry.location.lat;
+  this.longitude = res.body.results[0].geometry.location.lng;
 }
 
 function searchWeather(query){
   const darkskyData = require('./data/darksky.json');
-  // const forecast = [];
   const forecast = darkskyData.daily.data.map(apple => new Weather(apple));
 
-  // console.log(weather);
   return forecast;
 }
 
